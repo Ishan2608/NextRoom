44dbafcc98dd37e8afd7e941a96f121a4406abb6
= <ishan.developerguide@gmail.com>
2026-02-19 17:35:20 +0530

    Notify other users when new one joines and display other users on UI for each dynamically

--- index.js
+++ index.js
@@ -15,6 +15,40 @@ var server = app.listen(3000, () => {
 
 var io = socket(server);
 
+var userConnection = [];
+
 io.on("connection", (socket) => {
   console.log("New user connected to socket" + socket.id);
+  socket.on("userconnect", (data) => {
+    console.log(
+      "CLIENT:EMIT:userconnect = " + data.displayName + " and " + data.roomID,
+    );
+
+    var other_users = userConnection.filter(
+      (user) => user.meeting_id === data.roomID,
+    );
+
+    // ADDED: Send list of existing users to the newly joined user
+    other_users.forEach((user) => {
+      socket.emit("newuser_joined", {
+        other_user_id: user.user_id,
+        conn_id: user.connectionID,
+      });
+    });
+
+    // add the connected user to array of connected users.
+    userConnection.push({
+      connectionID: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.roomID,
+    });
+
+    // Let other users know about the new user
+    other_users.forEach((user) => {
+      socket.to(user.connectionID).emit("newuser_joined", {
+        other_user_id: data.displayName,
+        conn_id: socket.id,
+      });
+    });
+  });
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,92 @@
 var MyApp = (function () {
   var socket;
-  function init(uid, roomID) {
+  var userID;
+  var roomID;
+
+  function init(uid, roomId) {
+    userID = uid;
+    roomID = roomId;
     event_process_for_signaling_server();
   }
 
+  function addUser(other_user_id, conn_id) {
+    console.log(`User ${other_user_id} joined`);
+
+    var newJoinee = $("<div>", {
+      id: conn_id,
+      class: "joinee other",
+    }).append(
+      $("<img>", {
+        class: "joinee-pic",
+        alt: "",
+      }),
+      $("<h3>").text(other_user_id),
+      $("<video>", {
+        id: "v_" + conn_id,
+        autoplay: true,
+        playsinline: true,
+        style: "display: none;",
+      }),
+      $("<audio>", {
+        id: "a_" + conn_id,
+        autoplay: true,
+      }),
+    );
+
+    console.log("Appending new joinee to #joinees-horizontal");
+    $("#joinees-horizontal").append(newJoinee);
+    console.log("Total joinees:", $("#joinees-horizontal .joinee").length);
+  }
+
   function event_process_for_signaling_server() {
+    console.log("event_process_for_signaling_server called");
+    console.log("userID:", userID, "roomID:", roomID);
+
     socket = io.connect();
-    socket.on('connect', function () {
-      alert('Socket Connected to Client-Side');
-    })
+
+    socket.on("connect", function () {
+      console.log("Socket Connected to Client-Side " + socket.id);
+      console.log("socket.connected:", socket.connected);
+      console.log("Checking userID and roomID:", userID, roomID);
+
+      if (socket.connected) {
+        if (userID !== "" && roomID !== "") {
+          console.log("About to emit userconnect with:", {
+            displayName: userID,
+            roomID: roomID,
+          });
+
+          socket.emit("userconnect", {
+            displayName: userID,
+            roomID: roomID,
+          });
+
+          console.log("userconnect event emitted");
+        } else {
+          console.error(
+            "userID or roomID is empty! userID:",
+            userID,
+            "roomID:",
+            roomID,
+          );
+        }
+      } else {
+        console.error("Socket not connected!");
+      }
+    });
+
+    socket.on("newuser_joined", (data) => {
+      console.log("newuser_joined event received:", data);
+      addUser(data.other_user_id, data.conn_id);
+    });
+
+    socket.on("connect_error", (error) => {
+      console.error("Socket connection error:", error);
+    });
+
+    socket.on("disconnect", (reason) => {
+      console.log("Socket disconnected. Reason:", reason);
+    });
   }
 
   return {
